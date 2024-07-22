4c6262f3032fbea471da811bb6936b8852c0eb46
amnakhairi0 <amna.khairi@mail.utoronto.ca>
2024-07-22 12:34:32 -0400

    Finished Implementing Report Function
    User can now generate report by period, category, lot number or for all items with either all information or just picture and description

--- B07DemoSummer2024/app/src/main/java/com/example/b07demosummer2024/GenerateReport.java
+++ B07DemoSummer2024/app/src/main/java/com/example/b07demosummer2024/GenerateReport.java
@@ -89,6 +89,154 @@ public class GenerateReport {
         });
     }
 
+    public void generateReportByCategory(String category) {
+        collectionsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<Collection> collections = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Collection collection = snapshot.getValue(Collection.class);
+                    if (collection != null) {
+                        Log.d("GenerateReport", "Fetched collection: " + collection.name);
+                        if (collection.category.equals(category)) {
+                            collections.add(collection);
+                        }
+                    }
+                }
+                createPdf(category, collections);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("GenerateReport", "Error fetching data", databaseError.toException());
+            }
+        });
+    }
+
+    public void generateReportByCategoryPD(String category) {
+        collectionsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<Collection> collections = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Collection collection = snapshot.getValue(Collection.class);
+                    if (collection != null) {
+                        Log.d("GenerateReport", "Fetched collection: " + collection.name);
+                        if (collection.category.equals(category)) {
+                            collections.add(collection);
+                        }
+                    }
+                }
+                createPdfPD(category, collections);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("GenerateReport", "Error fetching data", databaseError.toException());
+            }
+        });
+    }
+
+    public void generateReportByPeriod(String period) {
+        collectionsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<Collection> collections = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Collection collection = snapshot.getValue(Collection.class);
+                    if (collection != null) {
+                        Log.d("GenerateReport", "Fetched collection: " + collection.name);
+                        if (collection.period.equals(period)) {
+                            collections.add(collection);
+                        }
+                    }
+                }
+                createPdf(period, collections);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("GenerateReport", "Error fetching data", databaseError.toException());
+            }
+        });
+    }
+
+    public void generateReportByPeriodPD(String period) {
+        collectionsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<Collection> collections = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Collection collection = snapshot.getValue(Collection.class);
+                    if (collection != null) {
+                        Log.d("GenerateReport", "Fetched collection: " + collection.name);
+                        if (collection.period.equals(period)) {
+                            collections.add(collection);
+                        }
+                    }
+                }
+                createPdfPD(period, collections);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("GenerateReport", "Error fetching data", databaseError.toException());
+            }
+        });
+    }
+
+    public void generateReportAll() {
+        collectionsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<Collection> collections = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Collection collection = snapshot.getValue(Collection.class);
+                    if (collection != null) {
+                        Log.d("GenerateReport", "Fetched collection: " + collection.name);
+                        collections.add(collection);
+                    }
+                }
+                if (!collections.isEmpty()) {
+                    createPdf("All collections", collections);
+                } else {
+                    Log.d("GenerateReport", "No collections found to generate the report.");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("GenerateReport", "Error fetching data", databaseError.toException());
+            }
+        });
+    }
+
+    public void generateReportAllPD() {
+        collectionsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<Collection> collections = new ArrayList<>();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Collection collection = snapshot.getValue(Collection.class);
+                    if (collection != null) {
+                        Log.d("GenerateReport", "Fetched collection: " + collection.name);
+                        collections.add(collection);
+                    }
+                }
+                if (!collections.isEmpty()) {
+                    createPdfPD("All collections", collections);
+                } else {
+                    Log.d("GenerateReport", "No collections found to generate the report.");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("GenerateReport", "Error fetching data", databaseError.toException());
+            }
+        });
+    }
+
 
     private void createPdf(String lotNumber, List<Collection> collections) {
         String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
@@ -141,6 +289,66 @@ public class GenerateReport {
             }
 
             document.close();
+            pdfDocument.close();
+            writer.close();
+            Log.d("GenerateReport", "PDF generated successfully");
+
+        } catch (IOException e) {
+            Log.e("GenerateReport", "Error generating PDF", e);
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    private void createPdfPD(String lotNumber, List<Collection> collections) {
+        String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+        File pdfFile = new File(pdfPath, "report.pdf");
+
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        try {
+            PdfWriter writer = new PdfWriter(pdfFile);
+            PdfDocument pdfDocument = new PdfDocument(writer);
+            Document document = new Document(pdfDocument);
+
+            document.add(new Paragraph("Report for (Lot/name/period/category): " + lotNumber + " (picture and description only)"));
+            document.add(new Paragraph(" "));
+
+            for (Collection collection : collections) {
+                Log.d("GenerateReport", "Adding collection to PDF: " + collection.name);
+                document.add(new Paragraph("Description: " + collection.description));
+                document.add(new Paragraph(" "));
+
+                if (collection.mediaUrl != null && !collection.mediaUrl.isEmpty()) {
+                    Future<Bitmap> future = executorService.submit(new Callable<Bitmap>() {
+                        @Override
+                        public Bitmap call() {
+                            try {
+                                return Picasso.get().load(collection.mediaUrl).get();
+                            } catch (IOException e) {
+                                Log.e("GenerateReport", "Error loading image from URL", e);
+                                return null;
+                            }
+                        }
+                    });
+
+                    try {
+                        Bitmap bitmap = future.get();
+                        if (bitmap != null) {
+                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                            Image img = new Image(ImageDataFactory.create(baos.toByteArray()));
+                            document.add(img);
+                        }
+                    } catch (InterruptedException | ExecutionException e) {
+                        Log.e("GenerateReport", "Error processing image future", e);
+                    }
+                }
+            }
+
+            document.close();
+            pdfDocument.close();
+            writer.close();
             Log.d("GenerateReport", "PDF generated successfully");
 
         } catch (IOException e) {
--- B07DemoSummer2024/app/src/main/java/com/example/b07demosummer2024/ReportActivity.java
+++ B07DemoSummer2024/app/src/main/java/com/example/b07demosummer2024/ReportActivity.java
@@ -6,12 +6,35 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ReportActivity extends AppCompatActivity {
-
+    //report by lot #
     private EditText editTextLotNumber;
     private Button btnGenerateLotReport;
 
+    //report by name
     private EditText editTextName;
     private Button btnGenerateNameReport;
+
+    //report by category
+    private EditText editTextCategory;
+    private Button btnGenerateCategoryReport;
+
+    //report by category
+    private EditText editTextPeriod;
+    private Button btnGeneratePeriodReport;
+
+    //report by category picture and description only
+    private EditText editTextCategoryPD;
+    private Button btnGenerateCategoryReportPD;
+
+    //report by period picture and description only
+    private EditText editTextPeriodPD;
+    private Button btnGeneratePeriodReportPD;
+
+    //report for all collections
+    private Button btnGenerateReport;
+
+    //report for all collections picture and description only
+    private Button btnGenerateReportPD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +48,28 @@ public class ReportActivity extends AppCompatActivity {
         //report by name
         editTextName = findViewById(R.id.editTextText2);
         btnGenerateNameReport = findViewById(R.id.button5);
+
+        //report by Category
+        editTextCategory = findViewById(R.id.editTextText3);
+        btnGenerateCategoryReport = findViewById(R.id.button7);
+
+        //report by Category (picture and description only)
+        editTextCategoryPD = findViewById(R.id.editTextText9);
+        btnGenerateCategoryReportPD = findViewById(R.id.button6);
+
+        //report by Period
+        editTextPeriod = findViewById(R.id.editTextText10);
+        btnGeneratePeriodReport = findViewById(R.id.button8);
+
+        //report by Category (picture and description only)
+        editTextPeriodPD = findViewById(R.id.editTextText11);
+        btnGeneratePeriodReportPD = findViewById(R.id.button11);
+
+        //report for all collections
+        btnGenerateReport = findViewById(R.id.button10);
+
+        //report for all collections Picture and description only
+        btnGenerateReportPD = findViewById(R.id.button12);
 
         btnGenerateLotReport.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +86,52 @@ public class ReportActivity extends AppCompatActivity {
                 generateReportByName(name);
             }
         });
+
+        btnGenerateCategoryReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String category = editTextCategory.getText().toString();
+                generateReportByCategory(category);
+            }
+        });
+
+        btnGenerateCategoryReportPD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String category = editTextCategoryPD.getText().toString();
+                generateReportByCategoryPD(category);
+            }
+        });
+
+        btnGeneratePeriodReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String period = editTextPeriod.getText().toString();
+                generateReportByPeriod(period);
+            }
+        });
+
+        btnGeneratePeriodReportPD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String period = editTextPeriodPD.getText().toString();
+                generateReportByPeriodPD(period);
+            }
+        });
+
+        btnGenerateReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateReportAll();
+            }
+        });
+
+        btnGenerateReportPD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateReportAllPD();
+            }
+        });
     }
 
     private void generateReportByLotNumber(String lotNumber) {
@@ -51,6 +142,36 @@ public class ReportActivity extends AppCompatActivity {
     private void generateReportByName(String name) {
         GenerateReport reportGenerator = new GenerateReport();
         reportGenerator.generateReportByName(name);
+    }
+
+    private void generateReportByCategory(String category) {
+        GenerateReport reportGenerator = new GenerateReport();
+        reportGenerator.generateReportByCategory(category);
+    }
+
+    private void generateReportByCategoryPD(String category) {
+        GenerateReport reportGenerator = new GenerateReport();
+        reportGenerator.generateReportByCategoryPD(category);
+    }
+
+    private void generateReportByPeriod(String period) {
+        GenerateReport reportGenerator = new GenerateReport();
+        reportGenerator.generateReportByPeriod(period);
+    }
+
+    private void generateReportByPeriodPD(String period) {
+        GenerateReport reportGenerator = new GenerateReport();
+        reportGenerator.generateReportByPeriodPD(period);
+    }
+
+    private void generateReportAll() {
+        GenerateReport reportGenerator = new GenerateReport();
+        reportGenerator.generateReportAll();
+    }
+
+    private void generateReportAllPD() {
+        GenerateReport reportGenerator = new GenerateReport();
+        reportGenerator.generateReportAllPD();
     }
 }
 
