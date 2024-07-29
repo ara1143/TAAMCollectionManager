f3aea76911dfa8b412f6e0c727225cb8a1fee43e
alizaasifb <aliza.asif@mail.utoronto.ca>
2024-07-29 14:07:00 -0400

    adding mockito dependency

--- B07DemoSummer2024/app/build.gradle.kts
+++ B07DemoSummer2024/app/build.gradle.kts
@@ -39,7 +39,9 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
+    testImplementation(libs.mockito.all)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.mockito.all)
 }
